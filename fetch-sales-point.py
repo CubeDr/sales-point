373fd595a2844fb07e528f25325e7da15cd3029c
Hyuni Kim <spaceship00@naver.com>
2024-04-02 12:36:26 +0000

    Add missing brace

--- fetch-sales-point.py
+++ fetch-sales-point.py
@@ -38,7 +38,7 @@ def fetch(url):
 def append(score):
     # Authentication
     github_token = os.getenv('ACCESS_TOKEN')
-    print('access token length: ' + str(len(github_token))
+    print('access token length: ' + str(len(github_token)))
     g = github.Github(github_token)
 
     repo = g.get_repo('CubeDr/sales-point')
